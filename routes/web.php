442ab1033cccaa5e0e3cdf446e273e4b245da2ee
Yohann Mahrez <yohann.mahrez@gmail.com>
2022-10-06 19:11:23 +0200

    Debut mise en place mySQL dans laravel OK (merci towej) en cours d'apprentissage ...

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $articles = [
-        [
-            'title' => "Mon premier article",
-            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit first. Quidem ipsam labore doloremque cum qui voluptatibus non aspernatur omnis, blanditiis delectus magnam placeat nihil facilis, quia laboriosam accusantium odio explicabo itaque. Rerum quibusdam adipisci temporibus dolor ad quod nemo repudiandae facere animi, dolorum reiciendis optio veniam!'
-        ],
-        [
-            'title' => "Mon deuxième article",
-            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ipsam labore doloremque cum qui voluptatibus non aspernatur omnis, blanditiis delectus magnam placeat nihil facilis, quia laboriosam accusantium odio explicabo itaque. Rerum quibusdam adipisci temporibus dolor ad quod nemo repudiandae facere animi, dolorum reiciendis optio veniam!'
-        ],
-        [
-            'title' => "Mon troisième article",
-            'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ipsam labore doloremque cum qui voluptatibus non aspernatur omnis, blanditiis delectus magnam placeat nihil facilis, quia laboriosam accusantium odio explicabo itaque. Rerum quibusdam adipisci temporibus dolor ad quod nemo repudiandae facere animi, dolorum reiciendis optio veniam!'
-        ]
-    ];
-    return view('home', [
-        'articles' => $articles
-    ]);
-});
+Route::get('/', [\App\Http\Controllers\PostController::class, 'index']);
 
 Route::get('hello-world', function () {
     return view('hello-world');
