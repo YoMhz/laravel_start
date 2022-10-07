63114b480cd0dc2496d36eb675f3896fd471c84d
Yohann Mahrez <yohann.mahrez@gmail.com>
2022-10-07 14:50:59 +0200

    Création d'un seeder PostTablesSeeder pour création contenu des article en cours ...

--- database/migrations/2022_10_06_150002_create_posts_table.php
+++ database/migrations/2022_10_06_150002_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title', 150);
-            $table->string('content');
+            $table->text('content');
             $table->timestamps();
         });
     }
