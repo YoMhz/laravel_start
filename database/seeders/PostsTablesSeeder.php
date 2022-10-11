f6f0fa86c303dde749c0d9d685cc81dd7ca0578b
Yohann Mahrez <yohann.mahrez@gmail.com>
2022-10-11 17:59:31 +0200

    Installation de Carbon et ajout dans la bose de donnéees created et updated_at OK

--- database/seeders/PostsTablesSeeder.php
+++ database/seeders/PostsTablesSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class PostsTablesSeeder extends Seeder
 {
@@ -18,12 +19,18 @@ class PostsTablesSeeder extends Seeder
         DB::table('posts')->insert([
         ['title' => "Le premier article",
             'content' => "Pour commencer voici le contenu du premier article",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ],
         ['title' => "Le second article",
             'content' => "Ensuite voici le contenu du second article",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ],
         ['title' => "Le troisieme article",
             'content' => "Et pour finir voici le contenu du troisième article",
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ],
         ]);
     }
